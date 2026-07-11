3b7fde0afe8c1d9df9d7dbc6aeeea0bd08fb5adb
dineshkanithi5 <dineshkanithi5@gmail.com>
2026-07-11 11:14:47 +0530

    Time: 12 ms (55.7%), Space: 19.5 MB (29.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        n=len(s)
-        m=len(t)
-        a=list(s)
-        b=list(t)
-        a.sort()
-        b.sort()
-        c=0
+        a={}
+        b={}
+        for i in s:
+            if i not in a:
+                a[i]=1
+            else:
+                a[i]+=1
+        for i in t:
+            if i not in b:
+                b[i]=1
+            else:
+                b[i]+=1
         if a==b:
             return True
         return False
@@ -15,6 +20,8 @@ s="anagram"
 t="nagaram"
 S=Solution()
 print(S.isAnagram(s,t))
-
-                
         
+s="anagram"
+t="nagaram"
+S=Solution()
+print(S.isAnagram(s,t))
